98255a5eea7d936f3cf64d43ab0db3a211995627
followmypeach <qizhiliao@gmail.com>
2026-08-23 15:25:27 +0800

    支持稍后再看页面的合作UP显示

--- src/page/watchalter.ts
+++ src/page/watchalter.ts
@@ -3,24 +3,29 @@ import { Comment } from "../core/comment";
 import { Like } from "../core/ui/like";
 import { user } from "../core/user";
 import { videoInfo } from "../core/video-info";
+import cssUplist from '../css/uplist.css';
 import html from '../html/watchlater.html';
-import { IAidDatail, jsonCheck } from "../io/api";
+import { IAidDatail, IStaf, jsonCheck } from "../io/api";
 import { addCss } from "../utils/element";
 import { urlObj } from "../utils/format/url";
 import { jsonpHook } from "../utils/hook/node";
 import { xhrHook } from "../utils/hook/xhr";
 import { poll } from "../utils/poll";
+import { Scrollbar } from "../utils/scrollbar";
 import { Header } from "./header";
 import { Page } from "./page";
 
 export class PageWatchlater extends Page {
     protected like: Like;
+    /** 合作UP主模式 */
+    protected _isStaffMode = false;
     constructor() {
         super(html);
         this.like = new Like();
         new Comment();
         this.toAv();
         this.enLike();
+        this.staffHook();
         this.toview();
         this.living();
         this.commentAgent();
@@ -87,5 +92,64 @@ export class PageWatchlater extends Page {
             const response = JSON.stringify(json)
             return { response, responseText: response, responseType: 'json' }
         })
+    }
+
+    /** 合作UP主 */
+    protected staffHook() {
+        jsonpHook('x/web-interface/view?', undefined, d => {
+            setTimeout(() => {
+                const data: any = jsonCheck(d).data;
+                if (user.userStatus!.staff && Array.isArray(data.staff) && data.staff.length) {
+                    this.staff(<IStaf[]>data.staff);
+                } else if (this._isStaffMode) {
+                    this.restoreSingleUp(data.owner);
+                }
+            });
+            return d;
+        }, false);
+    }
+
+    /** 合作UP主卡片 */
+    protected staff(staff: IStaf[]) {
+        poll(() => document.querySelector<HTMLElement>('.up-info-module'), upinfo => {
+            this._isStaffMode = true;
+            upinfo.style.display = 'none';
+            let container = document.querySelector<HTMLElement>('#v_upinfo_staff');
+            if (!container) {
+                container = document.createElement('div');
+                container.id = 'v_upinfo_staff';
+                container.className = 'up-info-m report-wrap-module report-scroll-module';
+                upinfo.parentNode?.insertBefore(container, upinfo.nextSibling);
+                addCss(cssUplist, "up-list");
+            }
+            let fl = '<span class="title">UP主列表</span><div class="up-card-box">';
+            fl = staff.reduce((s, d) => {
+                s = s + `<div class="up-card">
+                    <a href="//space.bilibili.com/${d.mid}" data-usercard-mid="${d.mid}" target="_blank" class="avatar">
+                    <img src="${d.face}@48w_48h.webp" /><!---->
+                    <span class="info-tag">${d.title}</span><!----></a>
+                    <div class="avatar">
+                    <a href="//space.bilibili.com/${d.mid}" data-usercard-mid="${d.mid}" target="_blank" class="${(d.vip && d.vip.status) ? 'name-text is-vip' : 'name-text'}">${d.name}</a>
+                    </div></div>`
+                return s;
+            }, fl) + `</div>`;
+            container.innerHTML = fl;
+            container.style.display = '';
+            const box = container.querySelector<HTMLElement>('.up-card-box');
+            box && new Scrollbar(box, true, false);
+        });
+    }
+
+    /** 合作UP切单UP */
+    protected restoreSingleUp(up: any) {
+        this._isStaffMode = false;
+        const upinfo = document.querySelector<HTMLElement>('.up-info-module');
+        const staffContainer = document.querySelector<HTMLElement>('#v_upinfo_staff');
+        if (staffContainer) {
+            staffContainer.style.display = 'none';
+        }
+        if (upinfo) {
+            upinfo.style.display = '';
+        }
     }
 }
